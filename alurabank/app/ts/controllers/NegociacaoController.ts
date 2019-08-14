5fb5def22cf6c7fde1760faddac9d15a3362f98a
Fernanda Aparecida Ferreira <fernanda.fereira@tegra.com.br>
2019-08-14 14:13:04 -0300

    finish aula 04

--- alurabank/app/ts/controllers/NegociacaoController.ts
+++ alurabank/app/ts/controllers/NegociacaoController.ts
@@ -1,6 +1,8 @@
 import { NegociacoesView, MensagemView } from '../views/index';
 import { Negociacoes, Negociacao } from '../models/index';
-import { domInject } from '../helpers/decorators/index';
+import { domInject, throttle } from '../helpers/decorators/index';
+import { NegociacaoParcial } from '../models/NegociacaoParcial';
+import { NegociacaoService } from '../services/index';
 
 export class NegociacaoController {
 
@@ -16,14 +18,13 @@ export class NegociacaoController {
     private _negociacoes = new Negociacoes();
     private _negociacoesView = new NegociacoesView('#negociacoesView');
     private _mensagemView = new MensagemView('#mensagemView');
+    private _service = new NegociacaoService();
 
     constructor() {
         this._negociacoesView.update(this._negociacoes);
     }
 
-    adiciona(event: Event) {
-
-        event.preventDefault();
+    adiciona() {
 
         let data = new Date(this._inputData.val().replace(/-/g, ','));
 
@@ -47,6 +48,27 @@ export class NegociacaoController {
 
     private _ehDiaUtil(data: Date) {
         return data.getDay() != DiaDaSemana.Sabado && data.getDay() != DiaDaSemana.Domingo
+    }
+
+    @throttle()
+    importaDados() {
+
+        this._service
+            .obterNegociacoes(res => {
+
+                if (res.ok) {
+                    return res;
+                }
+
+                throw new Error(res.statusText);
+            })
+            .then(negociacoes => {
+
+                negociacoes.forEach(negociacao =>
+                    this._negociacoes.adiciona(negociacao));
+
+                this._negociacoesView.update(this._negociacoes);
+            });
     }
 }
 
